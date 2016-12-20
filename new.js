738a29e52bf6ac95576afc440083abe076c73101
lopeterlo <b04705002@ntu.edu.tw>
2016-12-20 13:52:12 +0800

    12-20

--- new.js
+++ new.js
@@ -92,3 +92,28 @@ function call(){
       for (var i = 0; i <count; i++) {
   console.log(movielist)
 }         
+
+
+
+function search() {
+        var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
+
+
+
+
+
+
